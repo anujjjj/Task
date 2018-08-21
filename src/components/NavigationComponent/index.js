d08e74edbc1d177a502cc32fda2b74e2ea533d6b
anujjj <anuj10pande@gmail.com>
2018-08-21 15:04:49 +0530

    React=bootstap router added

--- src/components/NavigationComponent/index.js
+++ src/components/NavigationComponent/index.js
@@ -1,42 +1,37 @@
 import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Nav, NavItem } from 'react-bootstrap';
+
+
 import './style.css';
 import logo from '../../logo.svg';
-
+import { LinkContainer } from 'react-router-bootstrap';
 import PropTypes from 'prop-types';
 
 class NavigationComponent extends PureComponent {
   constructor(props) {
     super(props);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.state = {
-      activeKey: 1
-    };
-  }
-
-
-  handleSelect(selectedKey) {
-    this.setState({ activeKey: selectedKey });
   }
   render() {
-    console.log(this.state);
     return (
       <div>
         <Nav
           className="navigation"
           bsStyle="pills"
-          activeKey={this.state.activeKey}
-          onSelect={this.handleSelect} >
-          <NavItem eventKey={1} componentClass={Link} href="/introduction" to="/introduction">
-            <span className="glyphicon glyphicon-ok" ></span>
-            Introduction
-          </NavItem>
+        >
+          <LinkContainer to="/introduction">
+            <NavItem eventKey={1}>
+              <span className="glyphicon glyphicon-ok" ></span>
+              Introduction
+</NavItem>
+          </LinkContainer>
 
-          <NavItem eventKey={2} title="Item" componentClass={Link} href="/personal_information" to="/personal_information">
-            <span className="glyphicon glyphicon-ok" ></span>
-            Personal Information
-          </NavItem>
+          <LinkContainer to="/personal_information">
+            <NavItem eventKey={2}>
+              <span className="glyphicon glyphicon-ok" ></span>
+              Personal Information
+</NavItem>
+          </LinkContainer>
         </Nav>
       </div>
     );
@@ -47,4 +42,5 @@ NavigationComponent.propTypes = {
 
 };
 
-export default NavigationComponent;
+export default withRouter(NavigationComponent);
+
